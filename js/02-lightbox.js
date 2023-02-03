a1886a0945cbd7051b542df0aadc2afe746348f2
Tryhardie <realkalin@gmail.com>
2023-02-03 15:06:09 +0200

    hw-07 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+        <img class="gallery__image"
+        src="${image.preview}"
+        alt="${image.description}"/>
+      </a>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+const galleryLightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
