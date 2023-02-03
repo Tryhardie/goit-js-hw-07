e49cb7f14dedff23f37a7ec1181bdeee8a40516a
Tryhardie <realkalin@gmail.com>
2023-02-03 14:05:25 +0200

    first part of hw done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
+
+galleryRef.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const originalImgUrl = event.target.dataset.source;
+
+  const imgModal = basicLightbox.create(`<img src="${originalImgUrl}">`, {
+    onShow: (imgModal) => {
+      document.onkeydown = function (event) {
+        const isEscape = event.code === "Escape";
+
+        if (isEscape) {
+          imgModal.close();
+        }
+      };
+    },
+  });
+
+  imgModal.show();
+});
